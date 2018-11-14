97b4884a21361e84bcc4bedd75cc53d0a2be802b
Dan Ivy <danivy4@gmail.com>
2018-11-14 14:54:24 -0600

    Add CreateMeetupSelectDogs

---
+++ app/src/main/java/cs465/illinois/edu/dogonthequad/CreateMeetupSelectDogsActivity.java
@@ -0,0 +1,14 @@
+package cs465.illinois.edu.dogonthequad;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMeetupSelectDogsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_meetup_select_dogs);
+    }
+}
