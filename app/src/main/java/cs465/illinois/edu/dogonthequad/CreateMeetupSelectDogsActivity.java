b8df64e08a3d9785df5818839a98e482dbb7e632
Dan Ivy <danivy4@gmail.com>
2018-11-15 09:50:49 -0600

    Add dog list

--- app/src/main/java/cs465/illinois/edu/dogonthequad/CreateMeetupSelectDogsActivity.java
+++ app/src/main/java/cs465/illinois/edu/dogonthequad/CreateMeetupSelectDogsActivity.java
@@ -3,6 +3,13 @@ package cs465.illinois.edu.dogonthequad;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
 
 public class CreateMeetupSelectDogsActivity extends Activity {
 
@@ -10,5 +17,11 @@ public class CreateMeetupSelectDogsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meetup_select_dogs);
+
+        ArrayList<String> dogs = new ArrayList<>();
+        dogs.add("Fido");
+        ArrayAdapter<String> dogAdapter = new ArrayAdapter<>(this, R.layout.select_dog_list_item, dogs);
+        final ListView dogList = (ListView)findViewById(R.id.DogList);
+        dogList.setAdapter(dogAdapter);
     }
 }
