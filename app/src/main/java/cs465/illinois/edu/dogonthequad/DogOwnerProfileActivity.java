6d02cb762f7c57c2308503168c95a26c90b9acac
Nicolas Nytko <nicknytko@gmail.com>
2018-11-16 11:53:18 -0600

    add activity for dog owner profile

---
+++ app/src/main/java/cs465/illinois/edu/dogonthequad/DogOwnerProfileActivity.java
@@ -0,0 +1,12 @@
+package cs465.illinois.edu.dogonthequad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DogOwnerProfileActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_owner_profile);
+    }
+}
