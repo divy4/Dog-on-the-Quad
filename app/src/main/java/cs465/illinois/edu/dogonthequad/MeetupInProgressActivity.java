a48396d2b73e776491a19ceb6b774406d1da50b0
Nicolas Nytko <nicknytko@gmail.com>
2018-11-16 13:42:13 -0600

    create layout and base activity for "meetup in progress" screen

---
+++ app/src/main/java/cs465/illinois/edu/dogonthequad/MeetupInProgressActivity.java
@@ -0,0 +1,11 @@
+package cs465.illinois.edu.dogonthequad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeetupInProgressActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meetup_in_progress);
+    }
+}
