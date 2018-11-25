00f2c3f694c1f150e1767ce95cb4b5daabccd12f
Mark Schmidt <schmidtwmark@gmail.com>
2018-11-25 16:12:35 -0600

    Map looks like ass

--- app/src/main/java/cs465/illinois/edu/dogonthequad/CreateMeetupLocationActivity.java
+++ app/src/main/java/cs465/illinois/edu/dogonthequad/CreateMeetupLocationActivity.java
@@ -6,20 +6,41 @@ import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 
 import static cs465.illinois.edu.dogonthequad.MapActivity.MEETUP_KEY;
 
-public class CreateMeetupLocationActivity extends CreateMeetupActivity {
+public class CreateMeetupLocationActivity extends CreateMeetupActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initialize(R.layout.activity_create_meetup_location);
 
-        mMeetup.mLocation = new PointF(100, 200);
+        mMeetup.mLocation = new LatLng(40.107831, -88.227303);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng quad = new LatLng(40.107831, -88.227303);
+
+        googleMap.addMarker(new MarkerOptions().position(quad)
+                .title("Meetup Location").draggable(true));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(quad, (float) 8.0));
+    }
 }
 
