82d20a776df96473f8d5f6a1ef96f8c36524bd5f
Dan Ivy <danivy4@gmail.com>
2018-11-26 19:25:53 -0600

    Refactor meetup transfer functions to Util class

--- app/src/main/java/cs465/illinois/edu/dogonthequad/MapActivity.java
+++ app/src/main/java/cs465/illinois/edu/dogonthequad/MapActivity.java
@@ -35,9 +35,7 @@ public class MapActivity extends Activity implements View.OnClickListener {
             // Launch the create meetup location activity
             Intent intent = new Intent(this, CreateMeetupLocationActivity.class);
             Meetup meetup = new Meetup();
-            String json = new Gson().toJson(meetup);
-            Log.d(MEETUP_KEY,"Starting meetup creation, " + json);
-            intent.putExtra(MEETUP_KEY, json);
+            Util.addMeetupToIntent(intent, meetup);
             startActivity(intent);
         } else if (view.getId() == R.id.dog_profile_button) {
             /* Launch dog profile activity */
